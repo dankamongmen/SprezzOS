0508a9137a838a7650af2200f2c8ec89cfefd88d
nick black <dank@qemfd.net>
2012-04-05 16:58:26 -0400

    remove generated files

--- spl-0.6.0-rc7/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(spl):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="spl" command="/usr/bin/spl"
--- spl-0.6.0-rc7/debian/spl.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the spl package
-#
-0 4	* * *	root	[ -x /usr/bin/spl_maintenance ] && /usr/bin/spl_maintenance
