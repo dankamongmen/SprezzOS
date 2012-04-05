0724d0bb4f3307b56e413c46bb37131ffed1d8ca
nick black <dank@qemfd.net>
2012-04-05 15:35:04 -0400

    remove debian-installer from tree

--- debian-installer/manual/build/clear.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-basedir="$(cd "$(dirname $0)"; pwd)"
-manual_path="$(echo $basedir | sed "s:/build$::")"
-build_path="$manual_path/build"
-cd $build_path
-
-rm -rf build.out
-rm -rf build.tmp
