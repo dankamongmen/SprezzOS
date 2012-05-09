852131acf31905ead085a824c0cf1d4eac40a3ce
nick black <dank@qemfd.net>
2012-05-09 04:46:51 -0400

    kernel updates

--- zfs-0.6.0-rc8/debian/manpage.sgml.ex
+++ zfs-0.6.0-rc8/debian/manpage.sgml.ex
@@ -20,7 +20,7 @@ manpage.1: manpage.sgml
   <!ENTITY dhfirstname "<firstname>FIRSTNAME</firstname>">
   <!ENTITY dhsurname   "<surname>SURNAME</surname>">
   <!-- Please adjust the date whenever revising the manpage. -->
-  <!ENTITY dhdate      "<date>April 15, 2012</date>">
+  <!ENTITY dhdate      "<date>May  1, 2012</date>">
   <!-- SECTION should be 1-8, maybe w/ subsection other parameters are
        allowed: see man(7), man(1). -->
   <!ENTITY dhsection   "<manvolnum>SECTION</manvolnum>">
