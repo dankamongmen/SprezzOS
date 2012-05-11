2a85aadc636a5bc6457a29805edc1a0c9077a3f7
nick black <dank@qemfd.net>
2012-05-11 14:02:37 -0400

    remove a bunch of unnecessary garbage

--- spl-0.6.0-rc8/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(spl-modules-3.3.3-1-amd64-di):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="spl-modules-3.3.3-1-amd64-di" command="/usr/bin/spl-modules-3.3.3-1-amd64-di"
